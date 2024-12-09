c664d78c14d4ecd1b60c37878e0ecbd15a2651fd
Guilherme Hirle <guilherme.hirle1@gmail.com>
2024-12-08 22:50:40 -0300

    novo layout dos cards de serviços + novos serviços

--- automotive-services/src/app/app-routing.module.ts
+++ automotive-services/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServicesComponent } from './components/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'services', component: ServicesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
