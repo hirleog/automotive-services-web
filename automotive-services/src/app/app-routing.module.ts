0aa3082e8b54dfef21167421950b75822f1fc4df
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-01-14 20:51:54 -0300

    mudança no roteamento do app

--- automotive-services/src/app/app-routing.module.ts
+++ automotive-services/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
+import { HeroComponent } from './components/hero/hero.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ServicesComponent } from './components/services/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { ClientReviewsComponent } from './components/client-reviews/client-reviews.component';
 
 const routes: Routes = [
-  // { path: 'services', component: ServicesComponent },
+  { path: '', component: HeroComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'reviews', component: ClientReviewsComponent },
 ];
 
 @NgModule({
